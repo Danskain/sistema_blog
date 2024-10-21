c414f992f4cae6dacdad3675786e99ac6985ffde
Danska <david_08k@hotmail.com>
2024-10-21 01:21:54 -0500

    se agrega un cambio en el index para que no tengo el error de cors adicional se agrega dependencia de php para realizar prueabs unitarias phpunit

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 
+// Configuración de CORS para permitir solicitudes desde tu aplicación Angular
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require __DIR__ . '/vendor/autoload.php';
 //require '../vendor/autoload.php';
 // Cargar las variables de entorno desde el archivo .env
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 
 $dotenv->load();
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  // Responder a la solicitud de preflight
+  header("HTTP/1.1 200 OK");
+  exit();
+}
 
 require './src/Routes.php';
